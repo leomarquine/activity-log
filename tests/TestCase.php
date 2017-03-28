c75ebd690eee88d6c2b49b96f3202624f91a1e12
Leonardo Marquine <leonardomarquine@gmail.com>
2017-03-28 12:51:09 -0300

    Setup tests connection

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,55 @@
 <?php
 
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
 abstract class TestCase extends PHPUnit_Framework_TestCase
 {
-    
+    public function setUp()
+    {
+        $event = new Dispatcher(new Container);
+
+        $this->setUpDatabase($event);
+        $this->migrateTables();
+    }
+
+    protected function setUpDatabase($event)
+    {
+        $capsule = new Capsule;
+
+        $capsule->addConnection([
+            'driver'    => 'sqlite',
+            'database'  => ':memory:',
+        ]);
+
+        $capsule->setEventDispatcher($event);
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+    }
+
+    protected function migrateTables()
+    {
+        DB::schema()->create('users', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+
+        DB::schema()->create('activities', function($table) {
+            $table->increments('id');
+            $table->string('loggable_type');
+            $table->integer('loggable_id')->unsigned();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->string('event');
+            $table->json('before')->nullable();
+            $table->json('after')->nullable();
+            $table->timestamps();
+        });
+    }
+}
+
+class User extends \Illuminate\Database\Eloquent\Model {
+    protected $fillable = ['email', 'name'];
 }
