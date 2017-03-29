f6b248df07906faa333828b5013349ed4a951093
Leonardo Marquine <leonardomarquine@gmail.com>
2017-03-29 11:41:42 -0300

    Add missing config options

--- src/Marquine/Chronos/config/chronos.php
+++ src/Marquine/Chronos/config/chronos.php
@@ -4,6 +4,15 @@ return [
 
     'model' => Marquine\Chronos\Activity::class,
 
+    'diff' => [
+        'raw' => true,
+        'granularity' => 'word',
+        'hidden' => false,
+    ],
+
     'ignore' => ['id', 'created_at', 'updated_at', 'deleted_at'],
 
+    'loggable' => [
+        User::class => [],
+    ],
 ];
